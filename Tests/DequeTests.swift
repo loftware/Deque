bdc7fdb17b46624b45cbac5382f9bdb8fab4a1c9
Brennan Saeta <saeta@google.com>
2021-01-12 16:08:56 -0800

    Update copyrights.

--- Tests/DequeTests.swift
+++ Tests/DequeTests.swift
@@ -1,4 +1,4 @@
-// Copyright 2020 Penguin Authors
+// Copyright 2020 Brennan Saeta
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
